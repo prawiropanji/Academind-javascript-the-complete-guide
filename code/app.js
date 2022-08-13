10c02b6bd789c682822da27486c844bf06082d22
Panji Prawiro <prawiro.panji@gmail.com>
2022-08-13 15:43:30 +0700

    function factory

--- code/app.js
+++ code/app.js
@@ -1,9 +1,24 @@
-const add = (num1, num2) => {
-  return num1 + num2;
+// const add = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(add(9, 1));
+
+// const addRandom = (num1) => num1 + Math.random() * 10;
+
+// console.log(addRandom(1));
+
+const createTaxCalculator = (tax) => {
+  const calculateTax = (amount) => {
+    return amount * tax;
+  };
+
+  return calculateTax;
 };
 
-console.log(add(9, 1));
+const calculateAVTax = createTaxCalculator(0.1);
+const calculateIncomeTax = createTaxCalculator(0.05);
 
-const addRandom = (num1) => num1 + Math.random() * 10;
-
-console.log(addRandom(1));
+console.log(calculateAVTax(100));
+console.log(calculateAVTax(200));
+console.log(calculateIncomeTax(100));
