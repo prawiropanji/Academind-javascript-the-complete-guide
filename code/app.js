b99caf060dd1576a65eb24be56e3211df2f130bd
Panji Prawiro <prawiro.panji@gmail.com>
2022-08-19 10:40:53 +0700

    recursion function

--- code/app.js
+++ code/app.js
@@ -19,6 +19,35 @@ const createTaxCalculator = (tax) => {
 const calculateAVTax = createTaxCalculator(0.1);
 const calculateIncomeTax = createTaxCalculator(0.05);
 
-console.log(calculateAVTax(100));
-console.log(calculateAVTax(200));
-console.log(calculateIncomeTax(100));
+// console.log(calculateAVTax(100));
+// console.log(calculateAVTax(200));
+// console.log(calculateIncomeTax(100));
+
+let userName = 'max';
+
+let greetUser = () => {
+  console.log('hi ' + userName);
+};
+
+greetUser();
+
+userName = 'panji';
+
+console.log(this);
+
+// const powerOf = (x, n) => {
+//   let result = 1;
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+//   return result;
+// };
+
+const powerOf = (x, n) => {
+  if (n === 1) {
+    return x;
+  }
+  return x * powerOf(x, n - 1);
+};
+
+powerOf(2, 3);
