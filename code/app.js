abaa643863d630e2f9ee4d8d47d45adb3c26513a
Panji Prawiro <prawiro.panji@gmail.com>
2022-08-20 12:34:42 +0700

    async await and error handling

--- code/app.js
+++ code/app.js
@@ -1,35 +1,7 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
-function trackUserHandler() {
-  // navigator.geolocation.getCurrentPosition(
-  //   (position) => {
-  //     setTimeout(() => {
-  //       console.log(position);
-  //     }, 2000);
-  //   },
-  //   (data) => console.log(data)
-  // );
-  let posData;
-  getPosition()
-    .then((data) => {
-      posData = data;
-
-      return setTimer(2000);
-    })
-    .then((data) => {
-      console.log(data, posData);
-    });
-}
-
-button.addEventListener('click', trackUserHandler);
-
-// let counter = 0;
-// for (let i = 0; i < 100000000; i++) {
-//   counter += i;
-// }
-
-// console.log(counter);
+console.log('start');
 
 const getPosition = () => {
   let promise = new Promise((resolve, reject) => {
@@ -51,3 +23,42 @@ const setTimer = (time) => {
   });
   return promise;
 };
+
+async function trackUserHandler() {
+  // navigator.geolocation.getCurrentPosition(
+  //   (position) => {
+  //     setTimeout(() => {
+  //       console.log(position);
+  //     }, 2000);
+  //   },
+  //   (data) => console.log(data)
+  // );
+  // let posData;
+  // getPosition()
+  //   .then((data) => {
+  //     posData = data;
+
+  //     return setTimer(2000);
+  //   })
+  //   .then((data) => {
+  //     console.log(data, posData);
+  //   });
+  try {
+    let posData = await getPosition();
+    let message = await setTimer(4000);
+    console.log(message, posData);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+button.addEventListener('click', trackUserHandler);
+
+// let counter = 0;
+// for (let i = 0; i < 100000000; i++) {
+//   counter += i;
+// }
+
+// console.log(counter);
+
+console.log('end');
